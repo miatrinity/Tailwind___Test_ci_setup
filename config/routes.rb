600aa3642c7996bda265ed310cbf95a2417ea8e8
Mia Sinek && Trinity Takei <mia.trinity.codes@gmail.com>
2021-02-25 12:13:15 +0100

    Add verify route + namespace the test to make sure it doesn't collide with the app name

---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+  get 'verify/index', as: :verify_index
+end
--- test/system/tailwind_test.rb
+++ test/system/tailwind_test.rb
@@ -1,41 +1,43 @@
 require 'application_system_test_case'
 
-class TailwindTest < ApplicationSystemTestCase
-  TAILWIND_TEXT_RED_500 = 'rgb(239, 68, 68)'.freeze
-  TAILWIND_TEXT_4XL = '2.25rem'.freeze
+module RailsNewIo
+  class TailwindTest < ApplicationSystemTestCase
+    TAILWIND_TEXT_RED_500 = 'rgb(239, 68, 68)'.freeze
+    TAILWIND_TEXT_4XL = '2.25rem'.freeze
 
-  test 'Tailwind is Enabled' do
-    visit verify_index_path
+    test 'Tailwind is Enabled' do
+      visit verify_index_path
 
-    assert_selector 'h1', text: 'Hello, Tailwind!'
-    assert_equal computed_color, expected_tailwind_color, 'Error: color not computed correctly'
-    assert_equal computed_font_size, expected_tailwind_font_size, 'Error: font size not computed correctly'
-  end
+      assert_selector 'h1', text: 'Hello, Tailwind!'
+      assert_equal computed_color, expected_tailwind_color, 'Error: color not computed correctly'
+      assert_equal computed_font_size, expected_tailwind_font_size, 'Error: font size not computed correctly'
+    end
 
-  private
+    private
 
-  def computed_color
-    computed_style_for(selector: 'h1', attribute: 'color')
-  end
+    def computed_color
+      computed_style_for(selector: 'h1', attribute: 'color')
+    end
 
-  def expected_tailwind_color
-    TAILWIND_TEXT_RED_500
-  end
+    def expected_tailwind_color
+      TAILWIND_TEXT_RED_500
+    end
 
-  def computed_font_size
-    computed_style_for(selector: 'h1', attribute: 'font-size')
-  end
+    def computed_font_size
+      computed_style_for(selector: 'h1', attribute: 'font-size')
+    end
 
-  def expected_tailwind_font_size
-    font_size_in_pixels = (root_element_font_size.to_i * TAILWIND_TEXT_4XL.to_f).to_i
-    "#{font_size_in_pixels}px"
-  end
+    def expected_tailwind_font_size
+      font_size_in_pixels = (root_element_font_size.to_i * TAILWIND_TEXT_4XL.to_f).to_i
+      "#{font_size_in_pixels}px"
+    end
 
-  def root_element_font_size
-    computed_style_for(selector: 'html', attribute: 'font-size')
-  end
+    def root_element_font_size
+      computed_style_for(selector: 'html', attribute: 'font-size')
+    end
 
-  def computed_style_for(selector:, attribute:)
-    evaluate_script("window.getComputedStyle(document.querySelector('#{selector}'))['#{attribute}']")
+    def computed_style_for(selector:, attribute:)
+      evaluate_script("window.getComputedStyle(document.querySelector('#{selector}'))['#{attribute}']")
+    end
   end
 end
